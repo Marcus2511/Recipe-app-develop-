ba0ffccf488aa4d4e26812fcf6c711b0ccdcce57
github_Marcus2511 <ratofhamelin1125@gmail.com>
2023-01-09 18:47:48 +0700

    setup gh-pages

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 const Navbar =()=>{
       return (
             <div>
-                  <Link to="/Meal">Meal</Link>
-                  <Link to="/MealItem">MealItem</Link>
-                  <Link to="/RecipeIndex">RecipeIndex</Link>
-                  <Link to="/RecipeInfo">RecipeInfo</Link>
+                  <Link to="/Meal"></Link>
+                  <Link to="/MealItem"></Link>
+                  <Link to="/RecipeIndex"></Link>
+                  <Link to="/RecipeInfo"></Link>
             </div>
       )
 }
