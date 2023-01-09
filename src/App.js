bb1eb00c078ee2c004baa167345f1cb6b5864b5f
github_Marcus2511 <ratofhamelin1125@gmail.com>
2023-01-09 19:10:57 +0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import Meal from "./Components/routes/Meal";
 import { Routes, Route } from "react-router-dom";
-import RecipeInfo from "./Components/routes/RecipeInfo"
-import { BrowserRouter } from "react-router-dom";
+import RecipeInfo from "./Components/routes/RecipeInfo";
+import { HashRouter as Router } from "react-router-dom";
 import RecipeIndex from "./Components/routes/RecipeIndex";
 import MealItem from "./Components/routes/MealItem";
 import Navbar from "./Navbar";
 
-
 function App() {
   return (
     <div>
-        <Navbar/>
+      <Navbar />
+      <Router>
         <Routes>
           <Route path="/" element={<Meal />} />
           <Route path="/:MealId" element={<RecipeInfo />} />
         </Routes>
+      </Router>
     </div>
   );
 }
